451ce902670ec22c2012dc981c7ee7782840cc2c
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-01 18:49:26 -0600

    add get users

--- get_data_user/app.py
+++ get_data_user/app.py
@@ -1,11 +1,11 @@
 import json
 import pymysql
+import os
 
-rds_host = 'integradora-desarrollo.cd4gi2og06nk.us-east-2.rds.amazonaws.com'
-name = 'root'
-password = 'superroot'
-db_name = 'coauto'
-
+rds_host = os.environ['RDS_HOST']
+name = os.environ['DB_USERNAME']
+password = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_NAME']
 
 def lambda_handler(event, context):
     connection = pymysql.connect(
@@ -19,14 +19,16 @@ def lambda_handler(event, context):
 
     try:
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM user")
+            cursor.execute("SELECT id, email, phone_number, profile_image_url, role FROM user")
             result = cursor.fetchall()
 
             for row in result:
                 user = {
                     'id': row[0],
-                    'name': row[1],
-                    'email': row[2]
+                    'email': row[1],
+                    'phone_number': row[2],
+                    'profile_image_url': row[3],
+                    'role': row[4]
                 }
                 users.append(user)
 
